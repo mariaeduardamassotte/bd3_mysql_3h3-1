2bcab8381b8c263b0a69d54bd2d38c30ff4af724
mariaeduardamassotte <maria.massotte@etec.sp.gov.br>
2024-09-10 20:53:50 -0300

    branch com o código da aula do dia Tue, Sep 10, 2024  8:53:50 PM

---
+++ Aula0909 - Copia/SCRIPT_0910.sql
@@ -0,0 +1,52 @@
+select * FROM empregado;
+select * from departamento;
+select * from dependente;
+
+/*Query com Chaves Estrangeiras*/
+select
+	e.nome as empregado,
+    d.nome as dependente
+from
+	empregados e,
+    dependente d
+where
+e.cod_emp = d.cod_emp;
+
+/*Query com SubQuery*/
+
+select
+	d.*,
+    e.nome as empregado,
+	(format((datediff(now(), e.dt_nascimento) / 365), 0)) as idade,
+   (format((datediff(now(), e.dt_admissao) / 365), 0)) as tempo_empresa
+
+from
+	dependente d,
+    empregado e
+where
+	d.cod_emp in (
+    select cod_emp from empregado where (format((datediff(now(), dt_admissao) / 365), 0) < 22)
+    )
+and
+    e.cod_emp = d.cod_emp;
+    
+    /*View com uso de MySQL*/
+    select 
+		d.*,
+        (format((datediff(now(), e.dt_nascimento) / 365), 0)) as idade_dep,
+        e.nome as empregado,
+        (format((datediff(now(), e.dt_admissao) / 365), 0)) as tempo_empresa,
+        (e.salario * 0.15) as bonus_dependente,
+        "Desenvolvimento" as departamento        
+	from
+    empregado e,
+    dependente d
+    where
+    d.cod_emp in(
+			select cod_emp from empregado where cod_depto = 3
+            )
+	and
+		e.cod_emp;
+        
+        select * from vw_bonus_desenvolvimento;
+    
